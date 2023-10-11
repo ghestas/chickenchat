e4121097759369ca792d18f2dadf97915b64ba91
Ice_creme <141097782+ice-creme-studios@users.noreply.github.com>
2023-10-12 11:45:53 +1300

    made the login inputs

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Login from './components/Login';
 import Chat from './components/chat'
 
 function App() {
   return (
     <div className='app'>
-      <Chat/>
+      <Login />
     </div>
   );
 }
--- app/src/components/Login.jsx
+++ app/src/components/Login.jsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Login(){
+    const [logData, setLogData] = useState({
+        user: "",
+        pass: ""
+    })
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setLogData(prevFormData => {
+            return {
+                ...prevFormData,
+                [name]: value
+            }
+        })
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        console.log(logData)
+    }
+
     return(
         <div className="login">
             
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="username"
+                    onChange={handleChange}
+                    name="user"
+                    value={logData.user}
+                />
+                <input
+                    type="text"
+                    placeholder="password"
+                    onChange={handleChange}
+                    name="pass"
+                    value={logData.pass}
+                />
+                <button>Submit</button>
+            </form>
         </div>
     )
 }
