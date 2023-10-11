e62899a3acadca90cd47e0615f5e4671e9f21988
Ice_creme <141097782+ice-creme-studios@users.noreply.github.com>
2023-10-12 10:27:25 +1300

    templates

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import React, { useState } from 'react'
 
+import Chat from './components/chat';
+import Login from './components/Login';
+
+
 function App() {
   const [loggedIn, setLoggedIn] = useState(false)
 
   return (
     <div>
-      
+      {loggedIn ? <Chat /> : <Login />}
     </div>
   );
 }
