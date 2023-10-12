1abebc0bfbcd40e26a4296d18244dafe4d04a7f5
ghestas <ghestas@ghestas.com>
2023-10-13 11:48:45 +1300

    ummm isnt this just it...?

--- app/src/App.js
+++ app/src/App.js
@@ -13,9 +13,7 @@ function App() {
 
   return (
     <div className='app'>
-      {/* THIS CONTITIONAL RENDERING IS NOT WORKING */}
-      <Chat />
-      <Login log={Login}/>
+      {loggedIn === true ? <Chat /> : <Login log={Login}/>}
     </div>
   );
 }
